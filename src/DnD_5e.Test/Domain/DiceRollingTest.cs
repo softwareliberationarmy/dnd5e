9c4fde44da6ce5c1f8958c9e7f2bfb3ae6323768
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2020-07-15 20:22:46 -0500

    getting first unit test working

--- src/DnD_5e.Test/Domain/DiceRollingTest.cs
+++ src/DnD_5e.Test/Domain/DiceRollingTest.cs
@@ -11,16 +11,21 @@ namespace DnD_5e.Test.Domain
     {
         [Theory]
         [InlineData("1d6", 1, 6)]
+        [InlineData("2d6", 2, 12)]
+        [InlineData("1d4", 1, 4)]
         public async Task Dice_Roll_Falls_In_Range(string diceRollRequest, int minValue, int maxValue)
         {
             var target = new DieRoller();
             var roll = await target.Roll(diceRollRequest);
+            Console.WriteLine($"Rolls a {roll}");
             Assert.True(roll >= minValue && roll <= maxValue);
         }
     }
 
     public class DieRoller
     {
+        private static Random _random = new Random();
+
         public async Task<int> Roll(string requestString)
         {
             var parsedRequest = await Parse(requestString);
@@ -29,17 +34,63 @@ namespace DnD_5e.Test.Domain
 
         private async Task<int> ProcessRoll(DiceRollRequest parsedRequest)
         {
-            throw new NotImplementedException();
+            int result = parsedRequest.Modifier;
+            for (int i = 0; i < parsedRequest.Quantity; i++)
+            {
+                result += _random.Next(1, parsedRequest.Sides + 1);
+            }
+
+            return result;
         }
 
         private async Task<DiceRollRequest> Parse(string requestString)
         {
-            throw new NotImplementedException();
+            int qty = 0, sides = 0, modifier = 0;
+
+            var firstSplit = requestString.Split('d');
+            if (firstSplit.Length == 2 && int.TryParse(firstSplit[0].Trim(), out qty))
+            {
+                if (firstSplit[1].Contains("+"))
+                {
+                    var secondSplit = firstSplit[1].Split('+');
+                    if (secondSplit.Length == 2 
+                        && int.TryParse(secondSplit[0].Trim(), out sides) 
+                        && int.TryParse(secondSplit[1].Trim(), out modifier))
+                    {
+                        return new DiceRollRequest(qty, sides, modifier);
+                    }
+                }
+                else if (firstSplit[1].Contains("-"))
+                {
+                    var secondSplit = firstSplit[1].Split('-');
+                    if (secondSplit.Length == 2
+                        && int.TryParse(secondSplit[0].Trim(), out sides)
+                        && int.TryParse(secondSplit[1].Trim(), out modifier))
+                    {
+                        modifier *= -1;
+                        return new DiceRollRequest(qty, sides, modifier);
+                    }
+
+                }
+                else if(int.TryParse(firstSplit[1].Trim(), out sides))
+                {
+                    return new DiceRollRequest(qty, sides, modifier);
+                }
+            }
+
+            throw new FormatException("Unable to parse roll request");
         }
     }
 
     internal class DiceRollRequest
     {
+        public DiceRollRequest(in int qty, in int sides, in int modifier)
+        {
+            Quantity = qty;
+            Sides = sides;
+            Modifier = modifier;
+        }
+
         public int Quantity { get; set; }
         public int Sides { get; set; }
         public int Modifier { get; set; }
