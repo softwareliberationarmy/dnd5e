41f8262cf8bb890f3151cf77b1b2c04bd43e7cec
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2020-08-01 09:42:14 -0500

    copying roll creation logic over to character, which should know well enough what to roll, adding unit test and naive implementation (strength only)

---
+++ src/DnD_5e.Domain/Roleplay/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnD_5e.Domain.Roleplay
+{
+    public class Character
+    {
+        public int Strength { get; set; }
+
+        public string GetAbilityRoll(string ability)
+        {
+            var modifier = (Strength - 10) / 2;
+            var roll = "1d20" + (modifier > 0 ? "+" + modifier : modifier < 0 ? "-" + modifier : "");
+            return roll;
+        }
+    }
+}
---
+++ src/DnD_5e.Test/UnitTests/Domain/CharacterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DnD_5e.Domain.Roleplay;
+using Xunit;
+
+namespace DnD_5e.Test.UnitTests.Domain
+{
+    public class CharacterTests
+    {
+        [Fact]
+        public async void Returns_ability_check_roll_based_on_score()
+        {
+            var target = new Character
+            {
+                Strength = 16
+            };
+
+            Assert.Equal("1d20+3", target.GetAbilityRoll("strength"));
+        }
+    }
+}
