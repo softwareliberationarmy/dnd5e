88066383b2e31b92f5cc46fab7b618c3d9465e69
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2020-08-01 12:52:52 -0500

    making unit test data driven

--- src/DnD_5e.Test/UnitTests/Domain/CharacterTests.cs
+++ src/DnD_5e.Test/UnitTests/Domain/CharacterTests.cs
@@ -9,15 +9,25 @@ namespace DnD_5e.Test.UnitTests.Domain
 {
     public class CharacterTests
     {
-        [Fact]
-        public async void Returns_ability_check_roll_based_on_score()
+        [Theory]
+        [InlineData(10, "1d20")]
+        [InlineData(11, "1d20")]
+        [InlineData(12, "1d20+1")]
+        [InlineData(13, "1d20+1")]
+        [InlineData(14, "1d20+2")]
+        [InlineData(15, "1d20+2")]
+        [InlineData(16, "1d20+3")]
+        [InlineData(17, "1d20+3")]
+        [InlineData(18, "1d20+4")]
+        [InlineData(19, "1d20+4")]
+        public async void Returns_ability_check_roll_based_on_score(int score, string expectedRoll)
         {
             var target = new Character
             {
-                Strength = 16
+                Strength = score
             };
 
-            Assert.Equal("1d20+3", target.GetAbilityRoll("strength"));
+            Assert.Equal(expectedRoll, target.GetAbilityRoll("strength"));
         }
     }
 }
