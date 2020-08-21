294069d9866ee338bca350ffe7ce93549145574b
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2020-08-21 07:37:42 -0500

    making sure repository returns skill proficiencies

--- src/DnD_5e.Infrastructure/DataAccess/CharacterRepository.cs
+++ src/DnD_5e.Infrastructure/DataAccess/CharacterRepository.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using DnD_5e.Domain.Roleplay;
+using Microsoft.EntityFrameworkCore;
 
 namespace DnD_5e.Infrastructure.DataAccess
 {
@@ -16,20 +17,25 @@ namespace DnD_5e.Infrastructure.DataAccess
 
         public async Task<Character> GetById(int id)
         {
-            var record = await _context.Characters.FindAsync(id);
+            var record = await _context.Characters.Include(c => c.SkillProficiencies)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
             if (record == null)
             {
                 return null;
             }
             else
             {
+                var proficiencies = record.SkillProficiencies.Select(s =>
+                    (Skill.Type) s.Type);
                 return new Character(
                     new Ability(record.Strength, record.StrengthSaveProficiency),
                     new Ability(record.Dexterity, record.DexteritySaveProficiency),
                     new Ability(record.Constitution, record.ConstitutionSaveProficiency),
                     new Ability(record.Intelligence, record.IntelligenceSaveProficiency),
                     new Ability(record.Wisdom, record.WisdomSaveProficiency),
-                    new Ability(record.Charisma, record.CharismaSaveProficiency)
+                    new Ability(record.Charisma, record.CharismaSaveProficiency),
+                    proficiencies.ToArray()
                 );
             }
         }
