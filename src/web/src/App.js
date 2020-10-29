e93423484c8406341fc80fb42fb16a998d649fee
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2020-10-28 20:30:25 -0500

    adding input for specifying roll type and handling error codes

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import API from './components/api';
-import {Button} from 'react-bootstrap';
+import {Button, Form} from 'react-bootstrap';
 
 import logo from './logo.svg';
 import './App.css';
@@ -10,23 +10,40 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      roll: 0
+      roll: 0,
+      rollType: null,
+      rollError: null
     };
     
     this.rollButtonHandler = this.rollButtonHandler.bind(this);
-  }
-
-  componentDidMount(){
-
+    this.handleChange = this.handleChange.bind(this);
   }
 
   rollButtonHandler(){
-    API.get('roll/2d4p2')
-    .then(result => {
-      console.log('api',result);
-      const rollResult = result.data;
-      this.setState({ roll: rollResult.result});
-    });
+    if(this.state.rollType){
+      API.get('roll/' + this.state.rollType)
+      .then(result => {
+        console.log('api',result);
+        const rollResult = result.data;
+        this.setState({ roll: rollResult.result, rollError: null});
+      })
+      .catch(err => {
+        console.log('kp-error', err.response);
+        if(err.response.status === 400){
+          //bad request
+          this.setState({ rollError: 'You entered an invalid roll request.'})
+        }
+        else{
+          this.setState({rollError: 'Error rolling dice. Please try again.'})
+        }
+      });  
+    }
+  }
+
+  handleChange(event){
+    const partialState = {};
+    partialState[event.target.name] = event.target.value;
+    this.setState(partialState);
   }
 
   render(){
@@ -42,6 +59,11 @@ class App extends Component {
           <p>
             Roll your fate
           </p>
+          {this.state.rollError ? <Form.Label>{this.state.rollError}</Form.Label> : null}
+          <Form.Group className="d-flex d-inline" controlId="frmRoll">
+                        <Form.Label className="mr-3">Roll</Form.Label>
+                        <Form.Control as="input" name="rollType" onChange={this.handleChange} value={this.state.rollType}                         />
+          </Form.Group>
           <Button size="lg" onClick={this.rollButtonHandler} >Roll</Button>
         </header>
       </div>
