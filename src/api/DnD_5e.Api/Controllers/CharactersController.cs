c98f2ced6f618b25ad9c8ab108cc8781b7f08a3b
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-01-03 14:49:21 -0600

    Making api path values explicit, in case controller gets renamed

--- src/api/DnD_5e.Api/Controllers/CharactersController.cs
+++ src/api/DnD_5e.Api/Controllers/CharactersController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DnD_5e.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/characters")]
     [ApiController]
     public class CharactersController : ControllerBase
     {
--- src/api/DnD_5e.Api/Controllers/RollController.cs
+++ src/api/DnD_5e.Api/Controllers/RollController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DnD_5e.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/roll")]
     [ApiController]
     public class RollController : ControllerBase
     {
