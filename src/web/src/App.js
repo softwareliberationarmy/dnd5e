1e0aaaf0cc77731a2659c5b946d8494bc71a93bb
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-01-03 16:35:26 -0600

    Renaming Nav to NavMenu to avoid confusion with React bootstrap, and adding Characters page which shows up if user is authenticated.

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -3,20 +3,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import FreeRoller from './containers/FreeRoller/FreeRoller';
 import Home from './containers/Home/Home';
+import Characters from './containers/Characters/Characters';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/Nav/Nav';
+import NavMenu from './components/Nav/NavMenu';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Nav />
+          <NavMenu />
           <Switch>
             <Route path="/" exact component={ Home } />
             <Route path="/roll" component={ FreeRoller } />          
+            <Route path="/characters" component={ Characters } />
           </Switch>
         </div>
     </BrowserRouter>
--- src/web/src/components/Nav/NavMenu.js
+++ src/web/src/components/Nav/NavMenu.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Login from '../Auth/Login';
 import { Navbar, Nav } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react'
 
-const nav = () => {
+const NavMenu = () => {
+    const { isAuthenticated } = useAuth0();
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="sm">
@@ -11,7 +14,12 @@ const nav = () => {
                 <Navbar.Collapse id="basic-navbar-nav"> 
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/roll">Free Roll</Nav.Link>                        
+                        <Nav.Link href="/roll">Free Roll</Nav.Link>   
+                        {
+                            isAuthenticated ? 
+                            <Nav.Link href="/characters">Characters</Nav.Link> : 
+                            null  
+                        }                     
                     </Nav>
                     <Login />
                 </Navbar.Collapse>
@@ -20,4 +28,4 @@ const nav = () => {
     );
 }
 
-export default nav;
+export default NavMenu;
