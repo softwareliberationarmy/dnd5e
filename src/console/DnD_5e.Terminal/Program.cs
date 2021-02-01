ddc0d8cb3254d5145d8d5a939e0e623e57a96888
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-01-31 20:57:47 -0600

    getting a stub console app working that passes input to a mediatr handler. Adding exit words.

--- src/console/DnD_5e.Terminal/Program.cs
+++ src/console/DnD_5e.Terminal/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DnD_5e.Terminal
 {
@@ -7,7 +12,58 @@ namespace DnD_5e.Terminal
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, and welcome. This is D&D.");
+
+            await using (var provider = RegisterServices())
+            {
+                var mediator = provider.GetService<IMediator>();
+
+                bool result = true;
+                do
+                {
+                    Console.Write("DnD >> ");
+                    var input = Console.ReadLine();
+                    result = await mediator.Send(new InputRequest(input));
+
+                } while (result);
+            }
+
+            Console.WriteLine("Thank you for playing.");
+        }
+
+        private static ServiceProvider RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services.BuildServiceProvider(true);
         }
     }
+
+    public class InputRequest: IRequest<bool>
+    {
+        public string Input { get; }
+
+        public InputRequest(string? input)
+        {
+            Input = input ?? "";
+        }
+    }
+
+    public class InputRequestHandler : IRequestHandler<InputRequest,bool>
+    {
+        private static string[] _exitWords = new[] {"q", "quit", "exit"};
+
+        public async Task<bool> Handle(InputRequest request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine("User wrote {0}", request.Input);
+            if (_exitWords.Contains(request.Input.Trim().ToLower()))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
 }
+
+
