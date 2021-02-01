bb76f33a5dbdd087c971ac47ef7b25710c405071
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-01-31 18:21:14 -0600

    Creating console app project, renaming test project to indicate it's for API tests

---
+++ src/console/DnD_5e.Terminal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DnD_5e.Terminal
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
