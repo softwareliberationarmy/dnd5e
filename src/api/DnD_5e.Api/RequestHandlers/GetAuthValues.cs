1d2e50c76f5c269465677d750539bcdf5bfdb5bc
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-02-27 16:15:39 -0600

    adding request handler for get auth values and unit test, deleting empty authcontroller test class

---
+++ src/api/DnD_5e.Api/RequestHandlers/GetAuthValues.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DnD_5e.Api.Security;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace DnD_5e.Api.RequestHandlers
+{
+    public class GetAuthValues
+    {
+        public class Request : IRequest<Response>
+        {
+        }
+
+        public class Response
+        {
+            public string Domain { get; set; }
+            public string ClientId { get; set; }
+            public string Audience { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Request, Response>
+        {
+            private readonly Auth0Settings _settings;
+
+            public Handler(IOptions<Auth0Settings> options)
+            {
+                _settings = options.Value;
+            }
+
+            public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+            {
+                return new Response
+                {
+                    Domain = _settings.Domain,
+                    ClientId = _settings.ClientId,
+                    Audience = _settings.Audience
+                };
+            }
+        }
+    }
+}
---
+++ test/DnD_5e.Test.Api/UnitTests/Api/RequestHandlers/GetAuthValuesHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using DnD_5e.Api.RequestHandlers;
+using DnD_5e.Api.Security;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Moq.AutoMock;
+using Xunit;
+
+namespace DnD_5e.Test.Api.UnitTests.Api.RequestHandlers
+{
+    public class GetAuthValuesHandlerTests
+    {
+        [Fact]
+        public async Task Handle_Returns_Options_Values()
+        {
+            var mocker = new AutoMocker();
+            var fixture = new Fixture();
+            var settings = fixture.Create<Auth0Settings>();
+            mocker.GetMock<IOptions<Auth0Settings>>().SetupGet(s => s.Value).Returns(settings);
+
+            var target = mocker.CreateInstance<GetAuthValues.Handler>();
+            var result = await target.Handle(new GetAuthValues.Request(), CancellationToken.None);
+
+            result.Domain.Should().Be(settings.Domain);
+            result.ClientId.Should().Be(settings.ClientId);
+            result.Audience.Should().Be(settings.Audience);
+        }
+    }
+}
