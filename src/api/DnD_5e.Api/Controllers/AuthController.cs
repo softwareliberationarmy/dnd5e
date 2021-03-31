0719586cf5a94c8b5549940b18d7ee40e6d84639
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-03-30 19:33:50 -0500

    Adding HTTP response status codes to swagger documentation

--- src/api/DnD_5e.Api/Controllers/AuthController.cs
+++ src/api/DnD_5e.Api/Controllers/AuthController.cs
@@ -2,10 +2,12 @@
 using System.Threading.Tasks;
 using DnD_5e.Api.RequestHandlers.Auth;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DnD_5e.Api.Controllers
 {
+    [Produces("application/json")]
     [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
@@ -20,7 +22,11 @@ namespace DnD_5e.Api.Controllers
         /// <summary>
         /// Gets the Auth0 settings needed to authenticate a user
         /// </summary>
+        /// <response code="200">Settings were successfully recovered</response>
+        /// <response code="500">An error is encountered while obtaining the Auth0 settings</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<GetAuthValuesRequest.Response>> GetAuthConfigInfo()
         {
             return await _mediator.Send(new GetAuthValuesRequest(), CancellationToken.None);
--- src/api/DnD_5e.Api/Controllers/RollController.cs
+++ src/api/DnD_5e.Api/Controllers/RollController.cs
@@ -3,10 +3,12 @@ using System.Threading.Tasks;
 using DnD_5e.Api.RequestHandlers.Roll;
 using DnD_5e.Domain.Common;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DnD_5e.Api.Controllers
 {
+    [Produces("application/json")]
     [Route("api/roll")]
     [ApiController]
     public class RollController : ControllerBase
@@ -22,8 +24,12 @@ namespace DnD_5e.Api.Controllers
         /// Makes a random 1d20 roll
         /// </summary>
         /// <returns>a random roll result</returns>
+        /// <response code="200">Roll successfully made</response>
+        /// <response code="500">An error occurred during processing</response>
         // GET: api/<RollController>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<RollResponse>> Roll1d20()
         {
             //if no roll request is provided, assume the user wants a 1d20 roll
@@ -39,8 +45,14 @@ namespace DnD_5e.Api.Controllers
         /// </remarks>
         /// <param name="rollRequest">the requested roll</param>
         /// <returns>a random roll result</returns>
+        /// <response code="200">Roll successfully made</response>
+        /// <response code="400">Caller presented a roll request that could not be interpreted</response>
+        /// <response code="500">An error occurred during processing</response>
         // GET api/<RollController>/1d20
         [HttpGet("{rollRequest}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<RollResponse>> RollDice(string rollRequest)
         {
             try
@@ -59,8 +71,14 @@ namespace DnD_5e.Api.Controllers
         /// <param name="rollRequest">the requested roll</param>
         /// <param name="rollWith">'advantage' or 'disadvantage'</param>
         /// <returns>two random roll results, and an indicator of which one to use</returns>
+        /// <response code="200">Roll successfully made</response>
+        /// <response code="400">Caller presented a roll request that could not be interpreted</response>
+        /// <response code="500">An error occurred during processing</response>
         // GET api/<RollController>/1d20
         [HttpGet("{rollRequest}/{rollWith}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<RollResponse>> RollWith(string rollRequest, string rollWith = "")
         {
             try
