03feb17fba70632aed43be908d110429c7febdd1
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-05-16 16:11:14 -0500

    additional tests for NavMenu and cleanup of unused imports

--- src/web/src/components/Nav/NavMenu.test.js
+++ src/web/src/components/Nav/NavMenu.test.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import { render, screen, cleanup, waitFor, act, getByText } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import { useAuth0 } from '@auth0/auth0-react';
 
-import NavMenu from '../Nav/NavMenu';
+import NavMenu from './NavMenu';
 import Login from '../Auth/Login';
 
 jest.mock('@auth0/auth0-react');
 jest.mock('../Auth/Login');
 
 describe('Navigation menu', () => {
+    beforeEach(() => {
+        Login.mockReturnValue(null);
+        useAuth0.mockReturnValue({});
+    });
+
     afterEach(cleanup);
 
-    it('should show high level link to main website', () => {
-        useAuth0.mockReturnValue({});
-        Login.mockReturnValue(null);
+    it('should show high level link to main website every time', () => {
         render(<NavMenu />);
         expect(screen.getByText('My D&D App')).toBeDefined();
     });
 
+    it('should always show Home link', () => {
+        render(<NavMenu />);
+        expect(screen.getByText('Home')).toBeDefined();
+    });
+
+    it('should always show Free Roll link', () => {
+        render(<NavMenu />);
+        expect(screen.getByText('Free Roll')).toBeDefined();
+    });
+
     it('should show characters link if user is authenticated', () => {
         useAuth0.mockReturnValue({ isAuthenticated: true});
-        Login.mockReturnValue(null);
         render(<NavMenu />);
         expect(screen.getByText('Characters')).toBeDefined();
     });
 
     it('should not show characters link if user is not authenticated', () => {
-        useAuth0.mockReturnValue({ isAuthenticated: false});
-        Login.mockReturnValue(null);
         render(<NavMenu />);
         expect(screen.queryByText('Characters')).toBeNull();
     });
