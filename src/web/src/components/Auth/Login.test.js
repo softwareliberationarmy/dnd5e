4befdc84f63a36255f7e86a9c2efe3bca954a338
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-05-22 14:00:24 -0500

    final unit tests for login

--- src/web/src/components/Auth/Login.test.js
+++ src/web/src/components/Auth/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Login'
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import { useAuth0 } from '@auth0/auth0-react';
 import '@testing-library/jest-dom';
 
@@ -9,34 +9,54 @@ jest.mock('@auth0/auth0-react');
 describe('Login component', () => {
     afterEach(cleanup);
 
-    it('should show Log In button when user is not authenticated', () => {
-        useAuth0.mockReturnValue({});
-        render(<Login />);
-        expect(screen.getByRole('button')).toHaveTextContent('Log In');
+    describe('when the component loads',() => {
+        it('should show Log In button when user is not authenticated', () => {
+            useAuth0.mockReturnValue({});
+            render(<Login />);
+            expect(screen.getByRole('button')).toHaveTextContent('Log In');
+        });
+    
+        it('should show Log Out button when user is authenticated', () => {
+            useAuth0.mockReturnValue({ isAuthenticated: true, user: {} });
+            render(<Login />);
+            expect(screen.getByRole('button')).toHaveTextContent('Log Out');
+        });
+    
+        it('should show Loading link when user info is loading', () => {
+            useAuth0.mockReturnValue({ isLoading: true});
+            render(<Login />);
+            expect(screen.getByText('Loading ...')).toBeDefined();
+        });
+    
+        it('should show user info when not loading and user is authenticated', () => {
+            useAuth0.mockReturnValue({ isAuthenticated: true, user: { name: 'Bruce Wayne' }});
+            render(<Login />);
+            expect(screen.getByText('Bruce Wayne')).toBeDefined();
+        });
+    
+        it('should show error if error encountered', () => {
+            const expected = 'Danger, Will Robinson!';
+            useAuth0.mockReturnValue({ error: expected, isLoading: true });
+            render(<Login />);
+            expect(screen.getByText(expected)).toBeDefined();
+        });    
     });
 
-    it('should show Log Out button when user is authenticated', () => {
-        useAuth0.mockReturnValue({ isAuthenticated: true, user: {} });
-        render(<Login />);
-        expect(screen.getByRole('button')).toHaveTextContent('Log Out');
-    });
+    describe('interactions', () => {
+        it('should call logout function when clicking Log Out button', () => {
+            const logout = jest.fn();
+            useAuth0.mockReturnValue({ logout: logout, isAuthenticated: true, user: {} });
+            render(<Login />);
+            fireEvent.click(screen.getByText('Log Out'));
+            expect(logout.mock.calls.length).toBe(1);
+        });
 
-    it('should show Loading link when user info is loading', () => {
-        useAuth0.mockReturnValue({ isLoading: true});
-        render(<Login />);
-        expect(screen.getByText('Loading ...')).toBeDefined();
-    });
-
-    it('should show user info when not loading and user is authenticated', () => {
-        useAuth0.mockReturnValue({ isAuthenticated: true, user: { name: 'Bruce Wayne' }});
-        render(<Login />);
-        expect(screen.getByText('Bruce Wayne')).toBeDefined();
-    });
-
-    it('should show error if error encountered', () => {
-        const expected = 'Danger, Will Robinson!';
-        useAuth0.mockReturnValue({ error: expected, isLoading: true });
-        render(<Login />);
-        expect(screen.getByText(expected)).toBeDefined();
+        it('should call login function when clicking Log In button', () => {
+            const login = jest.fn();
+            useAuth0.mockReturnValue({loginWithRedirect: login });
+            render(<Login />);
+            fireEvent.click(screen.getByText('Log In'));
+            expect(login.mock.calls.length).toBe(1);
+        });
     });
 });
