00f23dbc46130c7a83b591a6c5ccbb79207256e3
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-06-12 14:43:50 -0500

    Adding click event to navigate to Character screen

--- src/web/src/containers/Characters/Characters.js
+++ src/web/src/containers/Characters/Characters.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Container, Row, Col } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 import { useToken } from '../../hooks/useToken';
 import { getMyCharacters } from '../../services/CharacterService';
@@ -8,6 +9,12 @@ const Characters = () => {
 
     const response = useToken(getMyCharacters);
 
+    const history = useHistory();
+
+    const goToCharacterPage = charId => {
+        history.push(`/characters/${charId}`);
+    }
+
     return (
         <>
         <h1>Characters</h1>
@@ -15,7 +22,7 @@ const Characters = () => {
             <Row>
             { (response && response.data) ? response.data.map(c => 
                 (
-                <Card as={Col} data-testid="character" md="3" className="m-1" text="dark" key={c.id}>
+                <Card as={Col} data-testid="character" md="3" className="m-1" text="dark" key={c.id} onClick={() => goToCharacterPage(c.id)}>
                     <Card.Body>
                         <Card.Title>{c.name}</Card.Title>                
                         <Card.Subtitle>Level {c.level} {c.class} ({c.race})</Card.Subtitle>
--- src/web/src/containers/Characters/Characters.test.js
+++ src/web/src/containers/Characters/Characters.test.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { act } from 'react-dom/test-utils';
 import { render, screen, cleanup, waitFor, getByText } from "@testing-library/react";
 
 import Characters from './Characters';
 import { getMyCharacters } from '../../services/CharacterService';
+import userEvent from '@testing-library/user-event';
+
+//NOTE: for testing useHistory: https://stackoverflow.com/questions/58524183/how-to-mock-history-push-with-the-new-react-router-hooks-using-jest/59451956
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
 jest.mock('../../services/CharacterService');
 jest.mock('@auth0/auth0-react');
 
 describe('Characters container', () => {
+
     beforeEach(() => {
+        mockHistoryPush.mockClear();
         useAuth0.mockImplementation(() => {
             return {
                 getAccessTokenSilently: () => Promise.resolve({})
@@ -50,6 +63,29 @@ describe('Characters container', () => {
             expect(characters.length).toBe(3);
             expect(characters[0].querySelector('.card-title').textContent).toBe('Bob');
             expect(characters[2].querySelector('.card-subtitle').textContent).toBe('Level 1 Cleric (Elf)');
+        });
+    });
+
+    it('should navigate to the specific character page when the user clicks on the character card', async () => {
+        getMyCharacters.mockImplementation(() => {
+            return Promise.resolve({
+                data: [
+                    { id: 31, name: 'Moonshine', level: 1, class: 'Cleric', race: 'Elf'}
+                ]
+            });
+        } );
+
+        await act(async () =>{ 
+            await render(<Characters />);     
+        });        
+        await waitFor(() => {
+            const characters = screen.getAllByTestId('character');
+            expect(characters.length).toBe(1);
+            userEvent.click(characters[0]);
+        });
+
+        await waitFor(() => {
+            expect(mockHistoryPush).toBeCalledWith('/characters/31');
         });
     });
 });
