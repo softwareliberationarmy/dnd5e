602edb11cea275301ff6c2fb616f79b0ae787cde
SoftwareLiberationArmy <softwareliberationarmy@gmail.com>
2021-07-05 13:02:19 -0500

    Finally able to mock out the character service methods being called inside useToken

--- src/web/src/containers/Characters/Character.test.js
+++ src/web/src/containers/Characters/Character.test.js
@@ -3,20 +3,25 @@ import { act } from "react-dom/test-utils";
 
 import Character from "./Character";
 import { useToken } from "../../hooks/useToken";
+import { getCharacter } from "../../services/CharacterService";
 
 jest.mock("../../hooks/useToken");
+jest.mock("../../services/CharacterService");
 
 const character = {
   name: "Zook Dangleripple",
   level: 1,
-  class: 'Fighter',
-  race: 'Gnome'
+  class: "Fighter",
+  race: "Gnome",
 };
 
 describe("Character page", () => {
-
   beforeEach(async () => {
-    useToken.mockImplementation(() => {
+    useToken.mockImplementation((func) => {
+      return func({});
+    });
+
+    getCharacter.mockImplementation(() => {
       return { data: character };
     });
 
@@ -26,27 +31,24 @@ describe("Character page", () => {
   });
 
   describe("basic information", () => {
-
     it("should show the character name", async () => {
       await waitFor(() => {
         expect(screen.getByText(character.name)).toBeDefined();
       });
     });
 
-    it('should show the character class, level, and race', async () => {
-        await waitFor(() => {
-            expect(screen.getByText('Level 1 Fighter (Gnome)')).toBeDefined();
-        });
-    });
-
-  });
-
-  describe('ability scores', () => {
-      it('should show character abilities', () => {
-
+    it("should show the character class, level, and race", async () => {
+      await waitFor(() => {
+        expect(screen.getByText("Level 1 Fighter (Gnome)")).toBeDefined();
       });
+    });
   });
-  
+
+  // describe('ability scores', () => {
+  //     it('should show character abilities', () => {
+
+  //     });
+  // });
 
   //should show character abilities
   //should allow user to click on an ability to make a roll
